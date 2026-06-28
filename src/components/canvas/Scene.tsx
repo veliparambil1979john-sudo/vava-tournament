d53a018e55e6cd137306b5c9d3176b8de40beb2f
sj2007ja <johnveliparambil18@gmail.com>
2026-06-28 12:58:20 +0530

    Change theme to yellow, red, white

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -48,14 +48,14 @@ function WorldCupTrophyImage() {
 
 export default function Scene() {
   return (
-    <div className="fixed inset-0 w-full h-full -z-10 bg-gradient-to-b from-[#1c003a] via-black to-[#0a0014] pointer-events-none">
+    <div className="fixed inset-0 w-full h-full -z-10 bg-gradient-to-br from-yellow-600 via-red-700 to-white pointer-events-none">
       <Canvas camera={{ position: [0, 0, 6], fov: 45 }}>
-        <ambientLight intensity={0.4} />
+        <ambientLight intensity={0.5} />
         
-        {/* World Cup 2026 Neon Lighting Scheme */}
-        <directionalLight position={[5, 10, 5]} intensity={1.5} color="#FF00FF" /> {/* Magenta */}
-        <directionalLight position={[-5, -10, -5]} intensity={1.5} color="#00FF00" /> {/* Neon Green */}
-        <pointLight position={[0, 3, 5]} intensity={2} color="#00FFFF" /> {/* Cyan highlight */}
+        {/* Yellow, Red, White Theme */}
+        <directionalLight position={[5, 10, 5]} intensity={2.5} color="#FFFF00" /> {/* Yellow */}
+        <directionalLight position={[-5, -10, -5]} intensity={2.5} color="#FF0000" /> {/* Red */}
+        <pointLight position={[0, 3, 5]} intensity={3} color="#FFFFFF" /> {/* White */}
         
         <Stars radius={100} depth={50} count={5000} factor={4} saturation={1} fade speed={1.5} />
         
